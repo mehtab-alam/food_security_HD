b86e2c38504b7beb9eb80972ccbfc522366d8997
mehtab-alam <mehtabalamshah@gmail.com>
2024-04-19 10:52:43 +0200

    updated data.sh to download big files

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 chmod -R 777 data.sh
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=14B4uEtMjXHxtyVqzwK2kvKIXIcNO3uwg' -O data.zip
+wget --no-check-certificate 'https://www.googleapis.com/drive/v3/files/1ai0jUWtc_-CPr_eFL6jO7OS2VDCt5bnU?alt=media&key=AIzaSyBo55XtefB47P_CPLKosGvnpEi3pQs5lCk' -O data.zip
 unzip data.zip
 rm data.zip
